ef05b90c806610fa5788960f09913b74236323d7
onkardtorane <145046802+onkadtorane@users.noreply.github.com>
2025-05-05 23:15:12 -0700

    Update SelectionSort.py

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,13 +1,17 @@
-def selectionSort(arr):
-	for i in range(len(arr)):
-		min = i+1
-        	for j in range(min,len(arr)):
-            		if arr[i]>arr[j]:
-            			arr[i],arr[j] = arr[j],arr[i]
-    	return arr
-    
-#print(selectionSort([89,56,45,34,65,76]))
+def selection_sort(arr):
+    n = len(arr)
 
-num=int(input("Enter Total number used in sorting:"))
-for i in range(0,num):
-	arr[i]=add(int(input("Enter NO you want Sort:")))
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+
+arr = [64, 25, 12, 22, 11]
+sorted_arr = selection_sort(arr)
+print("Sorted array:", sorted_arr)
