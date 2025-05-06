bec7ed907556787b37bd95eab44381e3e1c4e2d8
onkardtorane <145046802+onkadtorane@users.noreply.github.com>
2025-05-05 22:28:19 -0700

    Create external.py

---
+++ external.py
@@ -0,0 +1,7 @@
+print("Welcome To Google App Engine")
+print("We are performing one Basic Python program on Google App Engines Console")
+num=int(input("Enter any number:"))
+if num%2 == 0:
+  print("You had entered "+num+" is Even Number")
+else:
+  print("You had entered "+num+" is Odd Number")
