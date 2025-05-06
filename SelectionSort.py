a449cf4f27f891634837553143657fdca3e4339c
onkardtorane <145046802+onkadtorane@users.noreply.github.com>
2025-05-05 22:52:00 -0700

    Add files via upload

---
+++ SelectionSort.py
@@ -0,0 +1,14 @@
+def selectionSort(arr):
+	for i in range(len(arr)):
+		min = float('-inf')
+        	for j in range(i + 1, len(arr)):
+            		if arr[i] > arr[j]:
+                		arr[i],arr[j] = arr[j], arr[i]
+    	return arr
+    
+print(selectionSort([89,56,45,34,65,76]))
+
+num=int(input("Enter Total number used in sorting:"))
+for i in range(0,num):
+	arr[i].append(int(input("Enter NO you want Sort:")))
+	
